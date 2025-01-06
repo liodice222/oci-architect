9f29bad4ead0d97127acae55ec3ba9b0a7bc00f2
lea <115721978+liodice222@users.noreply.github.com>
2025-01-06 12:52:58 -0500

    prevent dom xss and sql injection

--- app/app.py
+++ app/app.py
@@ -3,5 +3,16 @@ from app.db import db
 
 app = create_app()
 
+@app.after_request
+def add_security_headers(response):
+    response.headers['Content-Security-Policy'] = (
+        "default-src 'self'; "  # Allow resources only from the same origin
+        "script-src 'self'; "  # Allow scripts only from the same origin
+        "style-src 'self' https://cdn.jsdelivr.net; "  # Allow styles from your origin and trusted CDNs
+        "img-src 'self' data:; "  # Allow images from your origin and inline data URIs
+        "object-src 'none';"  # Disallow embedded objects like Flash or PDFs
+    )
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
--- app/models/User.py
+++ app/models/User.py
@@ -6,6 +6,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    
 
 
